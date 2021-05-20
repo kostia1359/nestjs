f1d7470c765ba543627db5199ff8702bc49e3bab
Kostya <kostya@kostya.local>
2021-05-20 09:08:32 +0300

    Add documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Crypto API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  fs.writeFileSync('./documentation.json', JSON.stringify(document));
   await app.listen(3000);
 }
 bootstrap();
